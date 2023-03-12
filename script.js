84f59eb337b0f093ead5ffc4ce3187d7845cb912
Asilbek217 <yoldashaliyev0303@gmail.com>
2023-03-12 14:06:34 +0300

    !
    !

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-aa
+// 07:05:45PM
+
+function timeConversion(s) {
+  let amPm = s.charAt(8);
+  let military = "";
+  let hour = s.substring(0, 2);
+  let min = s.substring(2, 8);
+  if (amPm == "A") {
+    if (hour == "12") {
+      military = "00";
+    } else {
+      military = hour;
+    }
+  } else {
+    if (hour == "12") {
+      military = hour;
+    } else {
+      military = Number(hour) + 12;
+    }
+  }
+
+  return military + min;
+
+
+}
+
+console.log(timeConversion("07:05:45PM"));
